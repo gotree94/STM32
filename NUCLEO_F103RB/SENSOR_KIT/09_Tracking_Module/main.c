a08269555a84521a4a7b3baaba29310f0de8af15
KIM NAN WOO <49811923+gotree94@users.noreply.github.com>
2026-01-19 23:26:15 +0900

    Add files via upload

--- NUCLEO_F103RB/SENSOR_KIT/09_Tracking_Module/main.c
+++ NUCLEO_F103RB/SENSOR_KIT/09_Tracking_Module/main.c
@@ -1,29 +1,38 @@
+/* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Line Tracking Module Test for STM32F103 NUCLEO
- * @author         : 
- * @date           : 2025
- ******************************************************************************
- * @description
- * 라인 트래킹(추적) 모듈 테스트 (TCRT5000 기반)
- * - 적외선 센서를 이용한 흑/백 라인 감지
- * - 단일 채널 디지털 출력
- * - 3핀 모듈 (VCC, GND, OUT)
- * 
- * @pinout
- * - PA0  : Sensor Output (Digital Input)
- * - PA5  : LED Indicator (Output)
- * - PA2  : USART2 TX (Debug)
- * - PA3  : USART2 RX (Debug)
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 #include "stm32f1xx_hal.h"
 #include <stdio.h>
 #include <string.h>
+/* USER CODE END Includes */
 
-/* Private defines */
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
 #define SENSOR_PIN          GPIO_PIN_0
 #define SENSOR_PORT         GPIOA
 #define LED_PIN             GPIO_PIN_5
@@ -32,20 +41,29 @@
 /* Sensor state macros */
 #define LINE_DETECTED       0   // Active Low (흑색 라인 감지)
 #define LINE_NOT_DETECTED   1   // 백색 바닥
+/* USER CODE END PD */
 
-/* Private variables */
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
 
+/* USER CODE BEGIN PV */
 /* Statistics */
 uint32_t detect_count = 0;
 uint32_t total_samples = 0;
 uint32_t last_transition_time = 0;
 uint8_t prev_state = LINE_NOT_DETECTED;
+/* USER CODE END PV */
 
-/* Private function prototypes */
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
 uint8_t Read_Sensor(void);
 void Print_Sensor_Bar(uint8_t state);
 
@@ -54,91 +72,10 @@ int __io_putchar(int ch) {
     HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
     return ch;
 }
+/* USER CODE END PFP */
 
-/**
- * @brief  Main program
- */
-int main(void)
-{
-    HAL_Init();
-    SystemClock_Config();
-    MX_GPIO_Init();
-    MX_USART2_UART_Init();
-
-    printf("\r\n============================================\r\n");
-    printf("  Line Tracking Sensor Test (Single Channel)\r\n");
-    printf("  STM32F103 NUCLEO\r\n");
-    printf("============================================\r\n");
-    printf("Module: 3-Pin (VCC, GND, OUT)\r\n");
-    printf("PA0: Sensor Output\r\n");
-    printf("(0=Black Line, 1=White Surface)\r\n\r\n");
-
-    uint8_t sensor_state;
-    uint32_t last_print_time = 0;
-    uint32_t stats_time = 0;
-
-    while (1)
-    {
-        /* 센서 읽기 */
-        sensor_state = Read_Sensor();
-        total_samples++;
-
-        /* 라인 감지 시 LED ON */
-        if (sensor_state == LINE_DETECTED)
-        {
-            HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
-            detect_count++;
-        }
-        else
-        {
-            HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
-        }
-
-        /* 상태 전환 감지 */
-        if (sensor_state != prev_state)
-        {
-            uint32_t current_time = HAL_GetTick();
-            uint32_t duration = current_time - last_transition_time;
-            
-            printf("[TRANSITION] %s -> %s (after %lu ms)\r\n",
-                   prev_state == LINE_DETECTED ? "BLACK" : "WHITE",
-                   sensor_state == LINE_DETECTED ? "BLACK" : "WHITE",
-                   duration);
-            
-            last_transition_time = current_time;
-            prev_state = sensor_state;
-        }
-
-        /* 100ms마다 상태 출력 */
-        if (HAL_GetTick() - last_print_time >= 100)
-        {
-            last_print_time = HAL_GetTick();
-
-            printf("Sensor: [%s] ", 
-                   sensor_state == LINE_DETECTED ? "BLACK ###" : "WHITE ---");
-            Print_Sensor_Bar(sensor_state);
-            printf("\r\n");
-        }
-
-        /* 5초마다 통계 출력 */
-        if (HAL_GetTick() - stats_time >= 5000)
-        {
-            stats_time = HAL_GetTick();
-            uint32_t detect_percent = (detect_count * 100) / total_samples;
-            
-            printf("\r\n--- Statistics (5s) ---\r\n");
-            printf("Total samples: %lu\r\n", total_samples);
-            printf("Line detected: %lu (%lu%%)\r\n", detect_count, detect_percent);
-            printf("------------------------\r\n\r\n");
-            
-            detect_count = 0;
-            total_samples = 0;
-        }
-
-        HAL_Delay(10);
-    }
-}
-
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
 /**
  * @brief  Read Tracking Sensor
  * @retval LINE_DETECTED (0) or LINE_NOT_DETECTED (1)
@@ -162,80 +99,272 @@ void Print_Sensor_Bar(uint8_t state)
         printf("[----------]  <- NO LINE");
     }
 }
+/* USER CODE END 0 */
 
 /**
- * @brief System Clock Configuration (72MHz)
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  printf("\r\n============================================\r\n");
+  printf("  Line Tracking Sensor Test (Single Channel)\r\n");
+  printf("  STM32F103 NUCLEO\r\n");
+  printf("============================================\r\n");
+  printf("Module: 3-Pin (VCC, GND, OUT)\r\n");
+  printf("PA0: Sensor Output\r\n");
+  printf("(0=Black Line, 1=White Surface)\r\n\r\n");
+
+  uint8_t sensor_state;
+  uint32_t last_print_time = 0;
+  uint32_t stats_time = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      /* 센서 읽기 */
+      sensor_state = Read_Sensor();
+      total_samples++;
+
+      /* 라인 감지 시 LED ON */
+      if (sensor_state == LINE_DETECTED)
+      {
+          HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+          detect_count++;
+      }
+      else
+      {
+          HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
+      }
+
+      /* 상태 전환 감지 */
+      if (sensor_state != prev_state)
+      {
+          uint32_t current_time = HAL_GetTick();
+          uint32_t duration = current_time - last_transition_time;
+
+          printf("[TRANSITION] %s -> %s (after %lu ms)\r\n",
+                 prev_state == LINE_DETECTED ? "BLACK" : "WHITE",
+                 sensor_state == LINE_DETECTED ? "BLACK" : "WHITE",
+                 duration);
+
+          last_transition_time = current_time;
+          prev_state = sensor_state;
+      }
+
+      /* 100ms마다 상태 출력 */
+      if (HAL_GetTick() - last_print_time >= 100)
+      {
+          last_print_time = HAL_GetTick();
+
+          printf("Sensor: [%s] ",
+                 sensor_state == LINE_DETECTED ? "BLACK ###" : "WHITE ---");
+          Print_Sensor_Bar(sensor_state);
+          printf("\r\n");
+      }
+
+      /* 5초마다 통계 출력 */
+      if (HAL_GetTick() - stats_time >= 5000)
+      {
+          stats_time = HAL_GetTick();
+          uint32_t detect_percent = (detect_count * 100) / total_samples;
+
+          printf("\r\n--- Statistics (5s) ---\r\n");
+          printf("Total samples: %lu\r\n", total_samples);
+          printf("Line detected: %lu (%lu%%)\r\n", detect_count, detect_percent);
+          printf("------------------------\r\n\r\n");
+
+          detect_count = 0;
+          total_samples = 0;
+      }
+
+      HAL_Delay(10);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief GPIO Initialization
- */
-static void MX_GPIO_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    /* LED Pin - Output */
-    GPIO_InitStruct.Pin = LED_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
-
-    /* Sensor Pin - Input with Pull-up */
-    GPIO_InitStruct.Pin = SENSOR_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    HAL_GPIO_Init(SENSOR_PORT, &GPIO_InitStruct);
-}
-
-/**
- * @brief USART2 Initialization (115200 baud)
- */
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_USART2_UART_Init(void)
 {
-    __HAL_RCC_USART2_CLK_ENABLE();
 
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_2;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /* USER CODE BEGIN USART2_Init 0 */
 
-    GPIO_InitStruct.Pin = GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /* USER CODE END USART2_Init 0 */
 
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 115200;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    HAL_UART_Init(&huart2);
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
 }
-```
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
